dc208eff687654454352c625b797970c8d7fb3ec
Liubomyr Havrosh <lubgavrosh@gmail.com>
2024-02-28 21:31:57 +0100

    Committing changes before pull with rebase

--- DataAccess/Entities/User.cs
+++ DataAccess/Entities/User.cs
@@ -9,7 +9,11 @@ namespace Data.Entities
 {
     public class User : IdentityUser
     {
+<<<<<<< HEAD
    
+=======
+        override public string? Id {  get; set; }
+>>>>>>> 4ae25f8 (init new useer)
         public string? Name { get; set; }
         // --navigation properties
         public ICollection<Product> Products { get; set; }
