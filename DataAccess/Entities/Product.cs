895f829258b1f52cd34557f22b62cbdb8682f140
Liubomyr Havrosh <lubgavrosh@gmail.com>
2024-03-24 13:47:22 +0100

    fix bags

--- DataAccess/Entities/Product.cs
+++ DataAccess/Entities/Product.cs
@@ -17,8 +17,8 @@ namespace Data.Entities
 
      
         public decimal Price { get; set; }
-        public bool? Has_Delivery { get; set; }
-        public bool? Has_Photo { get; set; }
+        public bool Has_Delivery { get; set; }
+        public bool Has_Photo { get; set; }
         public decimal? Price_1 { get; set; }
 
         public decimal? Price_2 { get; set; }
